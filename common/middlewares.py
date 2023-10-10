3a27f4190c6ab500a6182e4b75f75a1901bbf089
wangrenyuan <renyuan.wang@zen-x.com.cn>
2023-10-10 18:49:29 +0800

    update

---
+++ common/middlewares.py
@@ -0,0 +1,10 @@
+"""
+Author: G
+Time: 2023 2023/10/10 10:36
+File: middlewares.py
+"""
+from common.response import StructuredResponse
+
+
+def pub_exception_handler(request, exc):
+    return StructuredResponse(content=str(exc), success=False, status_code=400)
